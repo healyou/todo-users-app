3450b6bb18b2fbf81de2bf0fc56990e339907498
Nikita Gorodilov <healyou1994@gmail.com>
2021-12-12 23:20:41 +0300

    postgresql

---
+++ src/main/java/ru/lappi/users/controller/UserController.java
@@ -0,0 +1,29 @@
+package ru.lappi.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Nikita Gorodilov
+ */
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + counter.incrementAndGet() + " " + String.format(template, name);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
